9c4c5650dafe41423b8ce141e825ca5c6c4496fd
Assad Isah <nottherealalanturing@gmail.com>
2022-11-20 21:00:04 +0100

    remove reactablejs

---
+++ src/components/desktop.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles/desktop.module.css';
+
+const Desktop = () => (
+  <div className={styles.desktopArea}>
+    <p>assad</p>
+  </div>
+);
+
+export default Desktop;
---
+++ src/components/taskbar.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './styles/taskbar.module.css';
+
+const Taskbar = () => <div className={styles.taskbar}>start</div>;
+
+export default Taskbar;
