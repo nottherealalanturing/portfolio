2c05331fe080b63ab842b4f47f19e1de2683b56f
Assad Isah <nottherealalanturing@gmail.com>
2022-12-06 23:32:25 +0100

    remove unused function

--- src/components/shutdown.js
+++ src/components/shutdown.js
@@ -2,23 +2,15 @@ import React from 'react';
 import styles from './styles/shutdown.module.css';
 import shutdown from '../assets/images/shutdown.png';
 
-const ShutDown = () => {
-  const close = () => {
-    window.opener = null;
-    window.open('', '_self');
-    window.close();
-  };
+const ShutDown = () => (
+  <div className={styles.shutdown}>
+    <img
+      src={shutdown}
+      alt="Shutdown"
+      className={styles.image}
+      draggable="false"
+    />
+  </div>
+);
 
-  setTimeout(() => close(), 3000);
-  return (
-    <div className={styles.shutdown}>
-      <img
-        src={shutdown}
-        alt="Shutdown"
-        className={styles.image}
-        draggable="false"
-      />
-    </div>
-  );
-};
 export default ShutDown;
